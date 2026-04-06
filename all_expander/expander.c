9c27a3bbe0627c725a6a21aea8f7d602fc0f7e7a
mathieu <mathieusuter@ahoo.com>
2026-04-06 15:53:51 +0200

    l'expander prend forme, les variables sont bien remplacees dans mon parser->arg, donc faudra l'appeler a chaque noeud, manque a gerer les quotes, les variables speciales, tout bien concatener et ce sera good

--- all_expander/expander.c
+++ all_expander/expander.c
@@ -1,11 +1,65 @@
 #include "../minishell.h"
 
-void	schr_in_env()
+void	replace(t_parser *parser, char *tmp, int j, int len)
 {
-	
+	char	*res;
+	int		new_size;
+	int		i;
+	int		k;
+	int		l;
+
+	k = 0;
+	i = 0;
+	l = 0;
+	new_size = ft_strlen(parser->arg[j]) + ft_strlen(tmp) - len;
+	res = malloc(sizeof(char) * new_size + 1);
+	while(parser->arg[j][i])
+	{
+		if (parser->arg[j][i] == '$')
+		{
+			i += len;
+			while (tmp[l])
+			{
+				res[k] = tmp[l];
+				k++;
+				l++;
+			}
+		}
+		else
+		{
+			res[k] = parser->arg[j][i];
+			k++;
+		}
+		i++;
+	}
+	res[k] = '\0';
+	free(parser->arg[j]);
+	parser->arg[j] = res;
 }
 
-int	is_in_var(char *str)
+char	*schr_in_env(char *var, char **envp)
+{
+	int	i;
+	int	j;
+	int	len;
+	char *res;
+
+	len = ft_strlen(var);
+	i = 0;
+	j = 0;
+	while (envp[i])
+	{
+		if (strncmp(var, envp[i], len) == 0)
+		{
+			res = ft_substr(envp[i], len, ft_strlen(envp[i] - len));
+			return (res);
+		}
+		i++;
+	}
+	return ("");
+}
+
+int	count_len(char *str)
 {
 	int	i;
 
@@ -17,25 +71,35 @@ int	is_in_var(char *str)
 	return (i);
 }
 
-char	*search_var(char **arg, char **envp)
+char	*search_var(t_parser *parser, char **envp)
 {
 	int		i;
 	int		j;
+	int		len;
 	char	*var;
+	char	*tmp;
 
 	i = 0;
 	j = 0;
-	while(arg[j])
+	while(parser->arg[j])
 	{
-		while (arg[j][i])
+		tmp = NULL;
+		while (parser->arg[j][i])
 		{
-			if (arg[j][i] == '$')
+			if (parser->arg[j][i] == '$')
 			{
 				i++;
-				var = ft_substr(arg[j], i, is_in_var(arg[j] + i));
+				len = count_len(parser->arg[j] + i);
+				tmp = ft_substr(parser->arg[j], i, len);
+				var = ft_strjoin(tmp, "=");
+				free (tmp);
+				tmp = schr_in_env(var, envp);
+				break;
 			}
 			i++;
 		}
+		if (tmp != NULL)
+			replace(parser, tmp, j, len);
 		j++;
 	}
 }
